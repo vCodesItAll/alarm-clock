1e4437a6dce2f7e33baba793c2d73c083c4fb3c5
Vanessa Dickey <vcdickey@gmail.com>
2023-09-15 02:30:12 +0000

    added function comment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 // DISPLAY current time in digital clock format
 
 // START 
+
+// DECLARE variables
+
+    // date OBJECT fetches time
+
+    // FUNCTION 
+        // EXECUTES when page loads
+
+        // DECLARE setInterval to pass seconds, 1000 ms = 1 second;
+        
