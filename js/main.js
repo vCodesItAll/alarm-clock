2533e518c9f10635024f6e3af0d9117498b69f1f
Vanessa Dickey <vcdickey@gmail.com>
2023-09-15 04:19:59 +0000

    lots of formatting

--- js/main.js
+++ js/main.js
@@ -1,51 +1,74 @@
+
+// //
+// // CREATE working alarm clock
+// /* ----------------------------------------------------------------- */
+// //
+// //
+// /* ----------------------------------------------------------------- */
+// // DECLARE variables
+
+//     // INITIALIZE currentTime as new Date()
+//     let currentTime = new Date();
+
+//     // INITIALIZE currentTime as new Date()
+//     let currentTime = new Date();
+
+
+//     // date OBJECT fetches time
+
+//     // FUNCTION 
+//         // EXECUTES when page loads
+
+//         // DECLARE setInterval to pass seconds, 1000 ms = 1 second;
+
+// /* ----------------------------------------------------------------- */
+// // SET alarm
+
+// // ALERT alarm 
+
+// // CREATE new alarm
+
+// // DISPLAY CURRENT TIME
+
+//     // EXECUTES when window.onload event executes
+    
+//     // function onLoadFunction(event) {
+//     //    console.log(event);
+//     // }
+
+// // DISPLAY ALARM STATUS
+// // DISPLAY ALARM TIME
+
+// // END
+
+//  IGNORE ABOVE OLD PSEUDOCODE ---------------------------------------
+
 // ------------------------------------------------------------------ //
 //                                                                    //
 //                         VARIABLE DICTIONARY                        //
 //                                                                    //
-// - currentTime is current time refreshing on load in HH:MM:SS         //
-// - alarmTime is the time the user sets for the alarm                  //
-//                                                                
+// - currentTime stores current time
+// - alarmTime is the time the user sets for the alarm                //
+//                                                                    //
+//                                                                    //
 // ------------------------------------------------------------------ //
 //
-//
-// CREATE working alarm clock
-/* ----------------------------------------------------------------- */
-//
-//
-/* ----------------------------------------------------------------- */
-// DECLARE variables
-
-    // INITIALIZE currentTime as new Date()
-    let currentTime = new Date();
-
-    // INITIALIZE currentTime as new Date()
-    let currentTime = new Date();
+// ------------------------------------------------------------------ //
+//                                                                    //
+//                         METHOD DICTIONARY                          //
+//                                                                    //
+//                //
+//                                                                    //
+//                                                                    //
+// ------------------------------------------------------------------ //
 
 
-    // date OBJECT fetches time
 
-    // FUNCTION 
-        // EXECUTES when page loads
 
-        // DECLARE setInterval to pass seconds, 1000 ms = 1 second;
+// START alarm clock program
 
-/* ----------------------------------------------------------------- */
-// SET alarm
+// INITIALIZE VARIABLES
+let currentTime = new Date();
+let alarmTime = new Date();
 
-// ALERT alarm 
-
-// CREATE new alarm
-
-// DISPLAY CURRENT TIME
-
-    // EXECUTES when window.onload event executes
-    
-    // function onLoadFunction(event) {
-    //    console.log(event);
-    // }
-
-// DISPLAY ALARM STATUS
-// DISPLAY ALARM TIME
-
-// END
-
+// DECLARE updateClock()
