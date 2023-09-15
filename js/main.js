fc03b347cd77b103aa1a158a833bde007d07b3d4
Vanessa Dickey <vcdickey@gmail.com>
2023-09-15 05:03:15 +0000

    added variables to dictionary

--- js/main.js
+++ js/main.js
@@ -44,13 +44,22 @@
 //  IGNORE ABOVE OLD PSEUDOCODE ---------------------------------------
 
 // ------------------------------------------------------------------ //
-//  /*                                                                  //
+//                                                                  //
 //                         VARIABLE DICTIONARY                        //
 //                                                                    //
 // - currentTime - date object stores current time
-// // - hours - int stores current hour in 12 hour format  /* 
+// // - hours - int stores current hour in 12 hour format  
 //     - minutes - int stores current minutes 
 //     - seconds - int stores current seconds
+ /* 
+- amOrPm - string that stores am or pm for current time 
+- alarmHour - int that stores user's chosen alarm hour
+- alarmMinute - int that stores user's chosen alarm minutes
+- amPm - string that stores user input of am or pm for alarm
+- timeNowIn12HourFormat - int that stores current hour in 12 hour format
+- alarmHour24 - int that stores alarm hour in 24 hour format
+- timeNowInSeconds - int that stores current time in seconds
+- alarmTimeInSeconds - int that stores the alarm time in seconds
 
 
 
